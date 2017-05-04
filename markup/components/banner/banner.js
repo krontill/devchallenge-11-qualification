69889c2e9318e5f7516eba8f3f5292a6c99e04f3
krontill <till.work@gmail.com>
2017-05-04 13:51:40 +0300

    Add scroll for arrow.

--- markup/components/banner/banner.js
+++ markup/components/banner/banner.js
@@ -1,1 +1,20 @@
+(function () {
+    let arrow = document.querySelector('[data-scroll="true"]');
 
+    function scrollArrow() {
+        let height = arrow.getBoundingClientRect().top + arrow.offsetHeight;
+        if (height > 20) {
+            window.scrollBy(0, 20);
+        } else {
+            window.scrollBy(0, height + 1);
+        }
+        if (height > 0) {
+            requestAnimationFrame(scrollArrow);
+        }
+    }
+
+    arrow.addEventListener('click', function () {
+        scrollArrow();
+    });
+
+})();
