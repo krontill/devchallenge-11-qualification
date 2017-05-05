5f6f6eaef03f9c33358c33364bdc9d7af86b55b7
krontill <till.work@gmail.com>
2017-05-05 09:26:27 +0300

    Add tabs info.

---
+++ markup/components/info/data/data.js
@@ -0,0 +1,19 @@
+var data = {
+    info: {
+        basic: {
+            text: 'basic',
+            time: '240',
+            cost: '1 500'
+        },
+        premium: {
+            text: 'premium',
+            time: '480',
+            cost: '1 666'
+        },
+        platinum: {
+            text: 'platinum',
+            time: '666',
+            cost: '1 750'
+        }
+    }
+};
