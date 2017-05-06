aeebb0c3d1c04d207fb8785e2b66fd3844bcd6cf
krontill <till.work@gmail.com>
2017-05-07 00:11:17 +0300

    Add minify html.

--- tars-config.js
+++ tars-config.js
@@ -43,7 +43,7 @@ module.exports = {
         "sounds": {},
         "taskFinishedText": "Task finished at: "
     },
-    "minifyHtml": false,
+    "minifyHtml": true,
     "generateStaticPath": true,
     "buildPath": "./dist/",
     "useBuildVersioning": true,
