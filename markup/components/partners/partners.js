cd857e948653de14f74c631531b04e66956076dc
krontill <till.work@gmail.com>
2017-05-06 16:33:01 +0300

    Add partners.

---
+++ markup/components/partners/partners.js
@@ -0,0 +1,9 @@
+(function () {
+    document.addEventListener('DOMContentLoaded', () => {
+        const slider3 = document.querySelector('.js_slider3');
+        lory(slider3, {
+            infinite: 6,
+            enableMouseEvents: true
+        });
+    });
+})();
